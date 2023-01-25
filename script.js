08ebbdf35d9a2ed611805af53eb04bf5646ba8e6
Yesusmac <jesusmac13@mail.com>
2023-01-24 23:36:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,224 @@
+let productos = [
+  {
+    id: 1,
+    nombre: "Arnes Pride",
+    categoria: "arneses",
+    precio: 5500,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-pride.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Arnes con cadenas",
+    categoria: "arneses",
+    precio: 7000,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-cadenas.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Arnes negro",
+    categoria: "arneses",
+    precio: 6000,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-negro.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Arnes rojo",
+    categoria: "arneses",
+    precio: 5500,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-rojo.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Cadenas Top",
+    categoria: "cadenas",
+    precio: 7000,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-top.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Arnes blanco abierto",
+    categoria: "arneses",
+    precio: 7000,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-blanco-abierto.png",
+  },
+  {
+    id: 7,
+    nombre: "Arnes Soulmate",
+    categoria: "arneses",
+    precio: 9000,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-Soulmate.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Arnes blanco",
+    categoria: "arneses",
+    precio: 6000,
+    stock: 20,
+    imgUrl: "./imagenes/productos/arnes-blanco.jpg",
+  },
+];
+
+let carrito = [];
+if (localStorage.getItem("carrito")) {
+  let carritoEnJSON = localStorage.getItem("carrito");
+  carrito = JSON.parse(carritoEnJSON);
+}
+
+let contenedorProductos = document.getElementById("contenedorProductos");
+let contenedorCarrito = document.getElementById("contenedorCarrito");
+let buscador = document.getElementById("buscador");
+let buscar = document.getElementById("buscar");
+buscar.onclick = filtrar;
+
+let inputMin = document.getElementById("min");
+let inputMax = document.getElementById("max");
+
+let verCarrito = document.getElementById("verCarrito");
+verCarrito.addEventListener("click", mostrarOcultarCarrito);
+
+function mostrarOcultarCarrito() {
+  contenedorProductos.classList.toggle("ocultar");
+  contenedorCarrito.classList.toggle("ocultar");
+}
+
+renderizarProductos(productos);
+renderizarCarrito(carrito);
+
+function finalizarCompra() {
+  localStorage.removeItem("carrito");
+  carrito = [];
+  renderizarCarrito(carrito);
+}
+
+function filtrar() {
+  let productosFiltrados;
+  if (buscador.value) {
+    productosFiltrados = productos.filter(
+      (producto) =>
+        producto.nombre.toLowerCase().includes(buscador.value.toLowerCase()) ||
+        producto.categoria.toLowerCase().includes(buscador.value.toLowerCase())
+    );
+  } else if (inputMin.value && inputMax.value) {
+    productosFiltrados = productos.filter(
+      (producto) =>
+        producto.precio > Number(inputMin.value) &&
+        producto.precio < Number(inputMax.value)
+    );
+  }
+  renderizarProductos(productosFiltrados);
+}
+
+function renderizarProductos(arrayDeProductos) {
+  contenedorProductos.innerHTML = "";
+  arrayDeProductos.forEach((producto) => {
+    let tarjetaProducto = document.createElement("div");
+    tarjetaProducto.classList.add("producto");
+    tarjetaProducto.id = `tarjeta${producto.id}`;
+
+    tarjetaProducto.innerHTML = `
+      <h3>${producto.nombre}</h3>
+      <p>$${producto.precio}</p>
+      <img src=${producto.imgUrl} />
+      <button id=agregar${producto.id}>Agregar al carrito</button>
+    `;
+    if (producto.stock < 10) {
+      tarjetaProducto.classList.add("pocoStock");
+      let pocasUnidades = document.createElement("p");
+      pocasUnidades.innerText = "Quedan pocas unidades";
+      tarjetaProducto.appendChild(pocasUnidades);
+    }
+
+    contenedorProductos.append(tarjetaProducto);
+
+    let boton = document.getElementById("agregar" + producto.id);
+    boton.onclick = agregarAlCarrito;
+  });
+}
+
+function agregarAlCarrito(e) {
+  let id = e.target.id.slice(7);
+  console.log(id);
+  let productoBuscado = productos.find((producto) => producto.id == id);
+  let productoEnCarrito = carrito.find(
+    (producto) => producto.id == productoBuscado.id
+  );
+
+  if (productoEnCarrito) {
+    let posicionProducto = carrito.findIndex(
+      (producto) => producto == productoEnCarrito
+    );
+    carrito[posicionProducto].unidades++;
+    carrito[posicionProducto].subtotal =
+      carrito[posicionProducto].precio * carrito[posicionProducto].unidades;
+  } else {
+    productoBuscado.unidades = 1;
+    productoBuscado.subtotal = productoBuscado.precio;
+    carrito.push(productoBuscado);
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+
+  renderizarCarrito(carrito);
+}
+
+function renderizarCarrito(productosEnCarrito) {
+  contenedorCarrito.innerText = "";
+  productosEnCarrito.forEach((producto) => {
+    let tarjetaProducto = document.createElement("div");
+    tarjetaProducto.classList.add("itemCarrito");
+    tarjetaProducto.innerHTML += `
+      <h3>${producto.nombre}</h3>
+      <p>${producto.unidades}</p>
+      <p>${producto.subtotal}</p>
+      <button onClick=eliminarProductoDelCarrito(eliminar${producto.id}) id=eliminar${producto.id}>Eliminar</button>
+    `;
+    contenedorCarrito.appendChild(tarjetaProducto);
+  });
+
+  contenedorCarrito.innerHTML += `
+    <button id="comprar">COMPRAR</button>
+  `;
+  let comprar = document.getElementById("comprar");
+  comprar.addEventListener("click", finalizarCompra);
+}
+
+function eliminarProductoDelCarrito(e) {
+  let id = e.id.substring(8);
+  let posProdAEliminar = carrito.findIndex((prod) => prod.id == id);
+  carrito.splice(posProdAEliminar, 1);
+  renderizarCarrito(carrito);
+}
+
+let checkBoxs = document.getElementsByClassName("inputFiltro");
+for (const checkBox of checkBoxs) {
+  checkBox.onclick = filtrarPorCategoria;
+}
+
+let ropa = document.getElementById("arneses");
+ropa.onclick = filtrarPorCategoria;
+
+let deporte = document.getElementById("cadenas");
+deporte.onclick = filtrarPorCategoria;
+
+function filtrarPorCategoria() {
+  let filtros = [];
+  for (const checkBox of checkBoxs) {
+    if (checkBox.checked) {
+      filtros.push(checkBox.id);
+    }
+  }
+  let productosFiltrados = productos.filter((producto) =>
+    filtros.includes(producto.categoria)
+  );
+  renderizarProductos(productosFiltrados);
+  if (filtros.length == 0) {
+    renderizarProductos(productos);
+  }
+}
